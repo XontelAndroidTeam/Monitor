0117628179ad0fdb16a6e20de9bd9f2359d94509
mohamedkhidr <midokhidr4@gmail.com>
2023-01-15 09:55:40 +0200

    UI with old nav design

---
+++ library/build/generated/source/buildConfig/release/org/easydarwin/video/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package org.easydarwin.video;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "org.easydarwin.video";
+  public static final String BUILD_TYPE = "release";
+  // Field from default config.
+  public static final boolean MEDIA_DEBUG = false;
+}
